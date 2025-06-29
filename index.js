641ad1a2e6827fd3076d3a8d1907daed6591150e
W-Kuria <william.njoroge.kuria@gmail.com>
2025-06-29 16:09:38 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+  name: "Ada Lovelace",
+  streetAddress: "123 Infinity Loop"
+};
+// updateEmployeeWithKeyAndValue()
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+
+//destructivelyUpdateEmployeeWithKeyAndValue()
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+//deleteFromEmployeeByKey()
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+//destructivelyDeleteFromEmployeeByKey()
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+
